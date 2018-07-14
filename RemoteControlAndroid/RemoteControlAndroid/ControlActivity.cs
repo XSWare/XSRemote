f6ed6b543ffa9b199b320456c3a7459c6d4d865f
CaptainOachkatzl <dave.hofer@hotmail.com>
2018-07-14 12:59:41 +0200

    added basic structure for control activity

---
+++ RemoteControlAndroid/RemoteControlAndroid/ControlActivity.cs
@@ -0,0 +1,18 @@
+﻿
+using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+
+namespace RemoteControlAndroid
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = false)]
+    public class ControlActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.activity_control);
+        }
+    }
+}
