6e02d7f108d1d6b6f3df5bb9c90a355831a11a7a
CaptainOachkatzl <dave.hofer@hotmail.com>
2018-07-20 18:32:33 +0200

    fixed media player next/previous

--- RemoteControlClient/RemoteControl/Functionalty/MediaPlayerHandler.cs
+++ RemoteControlClient/RemoteControl/Functionalty/MediaPlayerHandler.cs
@@ -4,27 +4,24 @@ namespace RemoteShutdown
 {
     class MediaPlayerHandler
     {
-        private const int APP_MESSAGE_PLAY_PAUSE = 0x00004978;
-        private const int MESSAGE_MEDIA_TRACK_SELECTION = 0xC;
-        private const int MESSAGE_NEXT = 0xB0000;
-        private const int MESSAGE_PREVIOUS = 0xC0000;
+        private const int MESSAGE_PLAY = 0x00004978;
+        private const int MESSAGE_NEXT = 0x0000497B;
+        private const int MESSAGE_PREVIOUS = 0x0000497A;
 
 
         public void StartStop()
         {
-            WindowsCalls.SendMessageW(GetMediaPlayerWindowHandle(), WindowsCalls.WM_COMMAND, (IntPtr)APP_MESSAGE_PLAY_PAUSE, IntPtr.Zero);
+            SendMediaMessage(MESSAGE_PLAY);
         }
 
         public void Next()
         {
-            WindowsCalls.PostMessage(GetMediaPlayerWindowHandle(), 0xC02B, (IntPtr)MESSAGE_MEDIA_TRACK_SELECTION, (IntPtr)MESSAGE_NEXT);
-            WindowsCalls.SendMessageW(GetMediaPlayerWindowHandle(), 0x8002, (IntPtr)0, (IntPtr)0);
+            SendMediaMessage(MESSAGE_NEXT);
         }
 
         public void Previous()
         {
-            WindowsCalls.PostMessage(GetMediaPlayerWindowHandle(), 0xC02B, (IntPtr)MESSAGE_MEDIA_TRACK_SELECTION, (IntPtr)MESSAGE_PREVIOUS);
-            WindowsCalls.SendMessageW(GetMediaPlayerWindowHandle(), 0x8002, (IntPtr)0, (IntPtr)0);
+            SendMediaMessage(MESSAGE_PREVIOUS);
         }
 
         private void SendMediaMessage(int message)
