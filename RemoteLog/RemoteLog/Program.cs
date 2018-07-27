0b72d2a01fa992807b30df8cdc773c227ed9eb6d
CaptainOachkatzl <dave.hofer@hotmail.com>
2018-07-27 13:51:15 +0200

    implemented prototype of RemoteLog

--- RemoteLog/RemoteLog/Program.cs
+++ RemoteLog/RemoteLog/Program.cs
@@ -1,15 +1,86 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Net;
 using System.Text;
-using System.Threading.Tasks;
+using XSLibrary.Cryptography.ConnectionCryptos;
+using XSLibrary.Network.Connections;
+using XSLibrary.Network.Connectors;
+using XSLibrary.ThreadSafety.Containers;
+using XSLibrary.Utility;
 
 namespace RemoteLog
 {
     class Program
     {
+        static LoggerConsolePeriodic logger = new LoggerConsolePeriodic("[LOCAL] ", "\n");
+        static LoggerConsolePeriodic connectionLogger = new LoggerConsolePeriodic("[LOCAL] ", "\n");
+        static LoggerConsole networkLog = new LoggerConsole();
+        static SafeList<IConnection> connections = new SafeList<IConnection>();
+
         static void Main(string[] args)
         {
+            logger.LogLevel = LogLevel.Information;
+            connectionLogger.LogLevel = LogLevel.Error;
+
+            Connect("127.0.0.1", 22224, "admin secure");
+
+            string command = "";
+            while ((command = Console.ReadLine()) != "exit")
+            {
+                string[] split = command.Split(' ');
+                if (split.Length != 4 || !Int32.TryParse(split[1], out int port))
+                {
+                    logger.Log(LogLevel.Priority, "Invalid command format.");
+                    continue;
+                }
+
+                string login = split[2] + " " + split[3];
+
+                Connect(split[0], port, login);
+            }
+
+            foreach(IConnection connection in connections.Entries)
+            {
+                connection.Disconnect();
+            }
+        }
+
+        private static void Connect(string ip, int port, string login)
+        {
+            EndPoint remote;
+            try { remote = new IPEndPoint(IPAddress.Parse(ip), port); }
+            catch
+            {
+                logger.Log(LogLevel.Priority, "Invalid IP format.");
+                return;
+            }
+
+            AccountConnector connector = new AccountConnector();
+            connector.Crypto = CryptoType.RSALegacy;
+            connector.Logger = logger;
+            connector.Login = login;
+
+            if (!connector.Connect(remote, out TCPPacketConnection connection))
+            {
+                Console.In.ReadLine();
+                return;
+            }
+
+            connections.Add(connection);
+            connection.Logger = connectionLogger;
+            connection.DataReceivedEvent += Connection_DataReceivedEvent;
+            connection.InitializeReceiving();
+            connection.OnDisconnect += HandleDisconnect;
+        }
+
+        private static void HandleDisconnect(object sender, EndPoint arguments)
+        {
+            connections.Remove(sender as IConnection);
+            networkLog.Log(LogLevel.Priority, "[{0}] Connection close by remote.", arguments);
+        }
+
+        private static void Connection_DataReceivedEvent(object sender, byte[] data, EndPoint source)
+        {
+            networkLog.Log(LogLevel.Priority, "[{0}] {1}", source, Encoding.ASCII.GetString(data));
         }
     }
 }
