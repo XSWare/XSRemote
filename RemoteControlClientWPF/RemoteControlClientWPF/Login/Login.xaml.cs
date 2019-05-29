0384bfe13de531a9c2e2a4ea636977d158d0ebae
CaptainOachkatzl <dave.hofer@hotmail.com>
2019-05-29 21:42:22 +0200

    initial commit of WPF-client

---
+++ RemoteControlClientWPF/RemoteControlClientWPF/Login/Login.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows.Controls;
+
+namespace RemoteControlClientWPF
+{
+    public partial class Login : UserControl
+    {
+        string Server
+        {
+            get { return m_txtServer.Text; }
+            set { m_txtServer.Text = value; }
+        }
+
+        string Username
+        {
+            get { return m_txtUser.Text; }
+            set { m_txtUser.Text = value; }
+        }
+
+        string Password
+        {
+            get { return m_txtPassword.Text; }
+            set { m_txtPassword.Text = value; }
+        }
+
+        public Login()
+        {
+            InitializeComponent();
+        }
+    }
+}
