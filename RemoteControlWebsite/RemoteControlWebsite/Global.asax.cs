5ef70c4620db9af88d73b26aaf8965a4fe72c4fb
CaptainOachkatzl <dave.hofer@hotmail.com>
2019-06-07 13:50:30 +0200

    set security protocol to tls1.2

--- RemoteControlWebsite/RemoteControlWebsite/Global.asax.cs
+++ RemoteControlWebsite/RemoteControlWebsite/Global.asax.cs
@@ -1,11 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Optimization;
 using System.Web.Routing;
-using System.Web.Security;
-using System.Web.SessionState;
 
 namespace RemoteControlWebsite
 {
@@ -14,6 +11,8 @@ namespace RemoteControlWebsite
         void Application_Start(object sender, EventArgs e)
         {
             // Code that runs on application startup
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
